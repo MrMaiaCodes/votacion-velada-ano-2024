ac9e821c0bea395f27ea0829dcfec567327fbbcc
MrMaia <mrmaiacodes@gmail.com>
2024-05-04 14:55:48 -0300

    wrote allowVote method

--- src/main/java/com/veladaano/programandoenjava/votacionveladaano2024/services/impl/VoteControl.java
+++ src/main/java/com/veladaano/programandoenjava/votacionveladaano2024/services/impl/VoteControl.java
@@ -1,4 +1,24 @@
 package com.veladaano.programandoenjava.votacionveladaano2024.services.impl;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 public class VoteControl {
+
+    private Map<String, Long> usersLatestVote = new HashMap<>();
+
+    public boolean allowVote(String user) {
+        long now = System.currentTimeMillis();
+        if(usersLatestVote.containsKey(user)) {
+            long latestVote = usersLatestVote.get(user);
+            if(now - latestVote < 1000) {
+                return false;
+            }
+        }
+        usersLatestVote.put(user, now);
+        return true;
+    }
 }
